4a1c5d61578bf13fee817b6c087fd56bb0637285
mofadl237 <mofadl237@gmail.com>
2025-06-15 17:23:18 +0300

    change Name And Add Icon

--- src/Components/ContentSyntaxHiglter.tsx
+++ src/Components/ContentSyntaxHiglter.tsx
@@ -1,20 +1,40 @@
+// import Editor from "react-simple-code-editor";
+// // import { highlight, languages } from "prismjs/components/prism-core";
+// import "prismjs/components/prism-markup";
+// import "prismjs/themes/prism.css";
+// import "prismjs/themes/prism.css";
+// import "prismjs/components/prism-css";
+// import { useDispatch, useSelector } from "react-redux";
+// import type { RootState } from "../app/store";
+// import {
+//   setClickedFileAction,
+//   updateOpenFileContent,
+// } from "../app/features/fileTreeSlice/fileTreeSlice";
+
+// import type { IFile } from "../interfaces";
+// import { setDataUpdate } from "../app/features/fileTreeData/fileTreeData";
+
+// import Prism from "prismjs";
+
+
 import Editor from "react-simple-code-editor";
-// import { highlight, languages } from "prismjs/components/prism-core";
+
+
+import Prism from "prismjs";
+
 import "prismjs/components/prism-markup";
-import "prismjs/themes/prism.css";
-import "prismjs/themes/prism.css";
 import "prismjs/components/prism-css";
+import "prismjs/themes/prism.css";
+
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState } from "../app/store";
 import {
   setClickedFileAction,
   updateOpenFileContent,
 } from "../app/features/fileTreeSlice/fileTreeSlice";
-
 import type { IFile } from "../interfaces";
 import { setDataUpdate } from "../app/features/fileTreeData/fileTreeData";
 
-import Prism from "prismjs";
 
 const ContentSyntax = () => {
   /**1- state */
